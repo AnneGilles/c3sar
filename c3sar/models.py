c71aee47363f6492fbf439167bb936bdd64bbcfa
Christoph Scheid <c@shri.de>
2011-09-22 14:12:31 +0200

    as of now

--- c3sar/models.py
+++ c3sar/models.py
@@ -4,6 +4,7 @@ import cryptacular.bcrypt
 
 # sqlalchemy stuff
 from sqlalchemy import (
+    Table,
     Column,
     ForeignKey,
 )
@@ -59,10 +60,11 @@ class EmailAddress(Base):
 
 #    user = relationship("User", backref=backref('addresses', order_by=id))
 
-    def __init__(self, email_address):
+    def __init__(self, email_address, is_confirmed=False):
         self.email_address = email_address
         self.confirm_code = a_random_string()
-        self.is_confirmed = False
+        #self.is_confirmed = False
+        self.is_confirmed = is_confirmed
 
     def __repr__(self):
         return "<Address('%s')>" % self.email_address
@@ -126,21 +128,16 @@ class User(Base): # ===========================================================
 #    def get_group_list(self):
 #        return [ str(group.name) for group in self.groups ]
 
-    def __init__(self, username, password, surname, lastname,
-                 email, email_conf, email_conf_code,):
+    def __init__(self, username, password, surname, lastname):
         self.username = username
         self.surname = surname
         self.lastname = lastname
-        self.email = email
-        self.email_conf = email_conf
-        self.email_conf_code = email_conf_code
         self.password = password
         self.date_registered = datetime.now()
         self.last_login = datetime.now()
 
     @classmethod
     def get_by_username(cls, username):
-        #from mortar_rdb import getSession
         dbSession = DBSession()
         return dbSession.query(cls).filter(cls.username==username).first()
 
@@ -186,30 +183,234 @@ class User(Base): # ===========================================================
 
     @classmethod
     def user_listing(cls, order_by, how_many=10):
-        #DBSession = getSession()
         q = DBSession.query(cls).all()
         # return q.order_by(order_by)[:how_many]
         return q
 
+
+class License(Base):
+    """
+    A license attributed to a work.
+    There is an infinite number of possible licenses,
+    so we better store them.
+    """
+    __tablename__ = 'licenses'
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode(255))
+    url = Column(Unicode(255))
+    author = Column(Unicode(255))
+
+    def __init__(self,
+                 name,
+                 url,
+                 author,
+                 ):
+        self.name = name
+        self.url = url
+        self.author = author
+
+    @classmethod
+    def get_by_license_id(cls, license_id):
+        return DBSession.query(cls).filter(cls.id==license_id).first()
+
+#    def license_listing(cls, order_by, how_many=10):
+    @classmethod
+    def license_listing(cls, how_many=10):
+        q = DBSession.query(cls).all()
+#        return q.order_by(order_by)[:how_many]
+        return q
+
+
+# table for relation between bands and members(=users)
+bands_members = Table('bands_members', Base.metadata,
+    Column('band_id', Integer, ForeignKey('bands.id'),
+        primary_key = True, nullable=False),
+    Column('user_id', Integer, ForeignKey('users.id'),
+        primary_key = True, nullable=False))
+
+# table for relation between bands and their tracks
+bands_tracks = Table('bands_tracks', Base.metadata,
+    Column('band_id', Integer, ForeignKey('bands.id'),
+        primary_key = True, nullable=False),
+    Column('tracks_id', Integer, ForeignKey('tracks.id'),
+        primary_key = True, nullable=False))
+
+# relation between licenses and tracks
+licenses_tracks = Table('licenses_tracks', Base.metadata,
+    Column('license_id', Integer, ForeignKey('licenses.id')),
+    Column('track_id', Integer, ForeignKey('tracks.id')))
+
+
+class Track(Base): ##########################################################
+    """
+    A Track. some music -- or just some metadata!
+    """
+    __tablename__ = 'tracks'
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode(255))
+    url = Column(Unicode(255))
+    album = Column(Unicode(255))
+    filepath = Column(Unicode(255))
+    bytesize = Column(Integer)
+    # track_composers = reference to User(s) or 'some text'
+    # track_lyrics = reference to User(s) or 'some text'
+
+    def __init__(self, name, album, url, filepath, bytesize):
+        self.name = name
+        self.album = album
+        self.url = url
+        self.filepath = filepath
+        self.bytesize = bytesize
+
+    @classmethod
+    def get_by_track_name(cls, track_name):
+        return DBSession.query(cls).filter(cls.name==track_name).first()
+
+    @classmethod
+    def get_by_track_id(cls, track_id):
+        return DBSession.query(cls).filter(cls.id==track_id).first()
+
+    @classmethod
+    def track_listing(cls, order_by, how_many=10):
+        q = DBSession.query(cls).all()
+        return q
+
+    # ToDo Album listing
+
+
+
+class Band(Base): #################################################### B A N D
+    """
+    A Band
+    consists of Artists
+    has Tracks
+    gets a Bucket --> ToDo
+    gets money in the long run
+    """
+    __tablename__ = 'bands'
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode(255))
+    homepage = Column(Unicode(255))
+    email = Column(Unicode(100))
+    registrar_id = Column(Integer, ForeignKey('users.id')) # who registered this band?
+    registrar = Column(Unicode(255))
+    date_registered = Column(DateTime(), nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
+    members = relationship(User,
+                           secondary=bands_members,
+                           backref="bands")
+    tracks =  relationship(Track,
+                           secondary=bands_tracks,
+                           backref="bands")
+    #band_registrar_rel = relation(User, cascade="delete", backref="bands")
+    # band_tracks = list of tracks...
+    # band_bucket ...
+    # band_bank_account ...
+
+    def __init__(self,
+                 name,
+                 email,
+                 homepage,
+                 registrar,
+                 registrar_id):
+
+        self.name = name
+        self.email = email
+        self.homepage = homepage
+        self.registrar = registrar
+        self.registrar_id = registrar_id
+        self.date_registered = datetime.now()
+
+    @classmethod
+    def get_by_band_name(cls, band_name):
+        return DBSession.query(cls).filter(cls.name==band_name).first()
+
+    @classmethod
+    def get_by_band_id(cls, band_id):
+        return DBSession.query(cls).filter(cls.id==band_id).first()
+
+    @classmethod
+    def band_listing(cls, order_by, how_many=10):
+        q = DBSession.query(cls).all()
+#        return q.order_by(order_by)[:how_many]
+        return q
+
+    @classmethod #find all bands a person has registered
+    def get_by_registrar_name(cls, registrar_name):
+        """
+        returns a list of bands
+        """
+        return DBSession.query(cls).filter(cls.registrar==registrar_name).all()
+
+
+
+class Playlist(Base): #######################################################
+    """
+    The Playlist definition. A Playlist is...
+
+    a list of tracks, containing
+     - the date they were played,
+    """
+    __tablename__ = 'playlists'
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode(255), unique=True)
+    # list of track ids .... = Column(Integer)
+
+    def __init__(self, name,):
+        self.name = name
+        #self.value = value
+
+
+
+
 def populate():
-    session = DBSession()
+    dbsession = DBSession()
 #    model = MyModel(name=u'root', value=55)
 #    session.add(model)
 #    session.flush()
 #    transaction.commit()
 
-    user1 = User(username=u'eins', surname=u'sur eins', lastname=u'last eins',
-                 email=u'eins@shri.de', email_conf=False, email_conf_code=u'QWERTZ',
-                 password=u'password')
+    user1 = User(username=u'firstUsername', surname=u'firstSurname',
+                 lastname=u'firstSurname',password=u'password')
+    user1.email_addresses = [
+        EmailAddress(email_address=u'first1@shri.de',is_confirmed=True),
+        EmailAddress(email_address=u'first2@shri.de'), ]
+    dbsession.add(user1)
 
-    user1.email_addresses = [ EmailAddress(email_address=u'c@shri.de'),
-                              EmailAddress(email_address=u'g@shri.de'), ]
-    session.add(user1)
+    user2 = User(username=u'secondUsername', surname=u'secondSurname',
+                 lastname=u'secondSurname',password=u'password')
+    user2.email_addresses = [
+        EmailAddress(email_address=u'second1@shri.de',is_confirmed=True),
+        EmailAddress(email_address=u'second2@shri.de'), ]
+    dbsession.add(user2)
+
+    band1 = Band(name=u"TestBand1", email=u"testband1@shri.de",
+                 homepage=u"http://testband.io", registrar="hans",
+                 registrar_id=1)
+    dbsession.add(band1)
+
+    band2 = Band(name=u"TestBand2", email=u"testband2@shri.de",
+                 homepage=u"http://testband.com", registrar="paul",
+                 registrar_id=2)
+    dbsession.add(band2)
+        
+    track1 = Track(name=u"TestTrack1", album=u'TestAlbum1',
+                   url=u'http://testband.io/t1.mp3', filepath=None,
+                   bytesize=None)
+    dbsession.add(track1)
+
+    track2 = Track(name=u"TestTrack2", album=u'TestAlbum2',
+                   url=u'http://testband.io/t2.mp3', filepath=None,
+                   bytesize=None)
+    dbsession.add(track2)
+
+
+        
 
     try:
-        session.flush()
+        dbsession.flush()
     except Exception, e: #PRAGMA: no cover
-        session.rollback()
+        dbsession.rollback()
         print "--- ROLLBACK cause:  -------------------------------------- "
         print str(e)
         print "----------------------------------------------------------- "
--- c3sar/views/user.py
+++ c3sar/views/user.py
@@ -7,17 +7,30 @@ from pyramid_simpleform.renderers import FormRenderer
 from pyramid_mailer import get_mailer
 from pyramid_mailer.message import Message
 
+from pyramid.httpexceptions import HTTPFound
+from pyramid.security import (
+    remember,
+    forget,
+    authenticated_userid,
+    )
+from pyramid.url import route_url
 from pyramid.view import view_config
 
+from c3sar.models import (
+    DBSession,
+    User,
+    EmailAddress,
+    )
 
+import re
+import random
+import string
 
-from c3sar.models import User
-
-
-## UniqueUsername
-# check if username already exists in database
 
 class UniqueUsername(validators.FancyValidator):
+    """
+    check if username already exists in database
+    """
     def _to_python(self, username, state):
         if User.get_by_username(username):
             raise formencode.Invalid(
@@ -25,27 +38,31 @@ class UniqueUsername(validators.FancyValidator):
         return username
 
 
-# formencode schema for user registration 
 class RegistrationSchema(formencode.Schema):
+    """
+    formencode schema for user registration
+    """
     allow_extra_fields = True
     username = formencode.All(validators.PlainText(not_empty = True),
                               UniqueUsername())
     password = formencode.validators.PlainText(not_empty = True)
-    email_address = formencode.validators.Email(resolve_domain = False, not_empty=True)
+    email = formencode.validators.Email(
+        resolve_domain = False, not_empty=True)
     surname =  formencode.validators.String(not_empty = True)
     lastname =  formencode.validators.String(not_empty = True)
-#    password =  formencode.validators.String(not_empty = True)
+    #password =  formencode.validators.String(not_empty = True)
     confirm_password =  formencode.validators.String(not_empty = True)
     chained_validators = [
         formencode.validators.FieldsMatch('password', 'confirm_password')
         ]
 
-
-## SecurePassword
-# as of http://formencode.org/Validator.html#writing-your-own-validator
-
-import re
 class SecurePassword(validators.FancyValidator):
+    """
+    check for the password to be secure
+
+    see
+    http://formencode.org/Validator.html#writing-your-own-validator
+    """
     #min = 8 #ToDo XXX
     min = 1
     non_letter = 1
@@ -75,11 +92,12 @@ class SecurePassword(validators.FancyValidator):
                                         non_letter=self.non_letter),
                           value, state)
 
-@view_config(route_name='register', 
+@view_config(route_name='register',
              permission='view',
              renderer='templates/user_register.pt')
 def user_register(request):
-    """ a user registers with the system
+    """
+    a user registers with the system
     """
 
     form = Form(request, RegistrationSchema)
@@ -88,13 +106,12 @@ def user_register(request):
     # create a random string for email verification procedure
     # http://stackoverflow.com/questions/2257441/
     #   python-random-string-generation-with-upper-case-letters-and-digits
-    import random
-    import string
     N=6
-    randomstring = ''.join(random.choice(string.ascii_uppercase 
+    randomstring = ''.join(random.choice(string.ascii_uppercase
                                          + string.digits) for x in range(N))
 
-    URL = "diogenes:6543"                       
+    URL = "diogenes:6543"
+    # ToDo XXX change this to be more generic
 
     if 'form.submitted' in request.POST and not form.validate():
         # form didn't validate
@@ -108,39 +125,41 @@ def user_register(request):
     if 'form.submitted' in request.POST and form.validate():
         # ready for registration!
         request.session.flash('form validated!')
-        #session = DBSession()
-        session = getSession()
+        session = DBSession()
+        #session = getSession()
         username = form.data['username']
-                           
+
 
         message = Message(subject = "C3S: confirm your email address",
                           sender = "noreply@c-3-s.org",
                           recipients = [form.data['email']],
-                          body = "Hello, " + form.data['user_surname'] + ", \n" 
+                          body = "Hello, " + form.data['surname'] + ", \n"
                           "Please confirm your email address by clicking this link: \n"
-                          "http://" + URL + "/user/confirm/" +  randomstring + "/" 
+                          "http://" + URL + "/user/confirm/" +  randomstring + "/"
                           + form.data['username'] + " \n"
                           "Thanks!")
         msg_accountants = Message(subject = "[C3S] new member registration",
                           sender = "noreply@c-3-s.org",
                           recipients = ['christoph@infinipool.com'],
-                          body = "Hello \n" 
+                          body = "Hello \n"
                           "A new member has registered with your site: \n"
                           "Username: " + form.data['username'] +  " \n"
                           "First name: " + form.data['surname'] +  " \n"
                           "Last name: " + form.data['lastname'] +  " \n"
                           "Email: " + form.data['email'] +  " \n"
                           "Thanks!")
-        
+
         user = User(
             username = username,
             password = form.data['password'],
             surname = form.data['surname'],
             lastname = form.data['lastname'],
-            email = form.data['email'],
-            email_conf_code = randomstring,
-            email_conf = False
             )
+
+        user.email_addresses = [
+            EmailAddress(email_address=form.data['email'])
+            ]
+
 #        user.groups.append('User')
         session.add(user)
 
@@ -165,9 +184,9 @@ def user_register(request):
         except:
             print "could not send email. no mail configured?"
 
-        # remember who this was == sign in user
+        # remember who this was == sign in user == log her in
         headers = remember(request, username)
-        
+
         redirect_url = route_url('home', request)
 
 
@@ -176,3 +195,273 @@ def user_register(request):
     return {
         'form': FormRenderer(form),
         }
+
+
+@view_config(route_name='confirm_email',
+             permission='view',
+             renderer='../templates/user_confirm_email.pt')
+# url structure: /user/confirm/{code}/{user_name}
+def user_confirm_email(request):
+    # values from URL/matchdict
+    conf_code = request.matchdict['code']
+    user_name = request.matchdict['user_name']
+    #get matching user from db
+    user = User.get_by_username(user_name)
+    print "--- in users.py:confirm_email: type(user): " + str(type(user))
+
+    # if database says already confirmed:
+    if user.email_conf == True:
+        #request.session.flash('Your email address is confirmed already!')
+        return { 'result_msg': "Your email is verified already!" }
+
+    #request.session.flash("code from db: " + user.user_email_conf_code)
+    #request.session.flash("is already conf'ed: " + str(user.user_email_conf))
+
+    result = (conf_code == user.user_email_conf_code)
+    if (result == True):
+        #request.session.flash("result is True")
+        result_msg = "your email has been successfully verified!"
+        # now set confirmed to True in db
+        user.user_email_conf = True
+        
+    else:
+        #request.session.flash("result is False")
+        result_msg = "Verification has failed. Bummer!"
+
+    return {
+        'result_msg': result_msg,
+        }
+
+######################################################## user_login
+class LoginSchema(formencode.Schema):
+    allow_extra_fields = True
+    username = formencode.validators.PlainText(not_empty=True)
+    password = formencode.validators.PlainText(not_empty=True)
+    
+
+#@view_config(route_name='login',
+#             permission='view',
+#             renderer='../templates/login.pt')
+def login_view(request):
+
+    came_from = "space"
+    headers = None
+    logged_in = authenticated_userid(request)
+    if logged_in is not None:
+        request.session.flash('you are logged in already!')
+        return HTTPFound(location = came_from, headers=headers)
+
+    # test for csrf token
+    #request.session.flash('token?: ' + str(request.POST.get("_csrf")))
+    # results in message: ['token?: d593dc44ff2012385df0abc5e371b4a5503b0c46'
+    
+    if logged_in is None:
+        request.session.pop_flash()
+
+    form = Form(request, LoginSchema)
+
+    home_view = route_url('home', request)
+    came_from = request.params.get('came_from', home_view)
+
+#    login_url = resource_url(request.context, request, 'login')
+#    login_url = request.url
+#    referrer = request.url
+    logged_in = authenticated_userid(request)
+    loggedin =  authenticated_userid(request)
+    request.session.flash(logged_in)
+
+#    if referrer == login_url:
+#        referrer = '/'
+
+#    came_from = request.params.get('came_from', referrer)
+
+    username = ''
+    message = ''
+    login = ''
+    password = ''
+
+    post_data = request.POST
+    #request.session.flash(post_data)
+
+    if not 'submit' in post_data:
+        request.session.flash('not submitted!')
+
+    if 'submit' in post_data and not form.validate():
+        request.session.flash(u'-- form didnt validate')
+
+    if 'submit' in post_data and form.validate():
+
+        login = post_data['username']
+        request.session.flash(u'username: ' + login)
+
+        password = post_data['password']
+        request.session.flash(password)
+
+        if User.check_password(login, password):
+            request.session.flash(u'User.check_password was True!')
+            request.session.flash(u'login: ' + login)
+            headers = remember(request, login)
+            request.session.flash(u'Logged in successfully.')
+            return HTTPFound(location = came_from, headers=headers)
+
+        else:
+            request.session.flash(u'User.check_password was NOT True!')
+            request.session.flash(u'account not found or passwords didnt match')
+
+    request.session.flash(u'Failed to login. Musta been errors!')
+    return {
+        'form': FormRenderer(form),
+        }
+
+############################################################ user_logout
+@view_config(permission='view',
+             route_name='logout'
+             )
+def logout_view(request):
+    request.session.invalidate()
+    request.session.flash(u'Logged out successfully.')
+    headers = forget(request)
+    return HTTPFound(location = route_url('home', request),
+                     headers = headers)
+
+############################################################ user_list
+@view_config(route_name='user_list',
+             permission='view',
+             renderer='../templates/user_list.pt')
+def user_list(request):
+    users = User.user_listing(User.id.desc())
+    return {'users': users}    
+
+#################################################################### user_view
+@view_config(route_name='user_view',
+             permission='view',
+             renderer='../templates/user_view.pt')
+def user_view(request):
+    user_id = request.matchdict['user_id']
+    user = User.get_by_user_id(user_id)
+
+    # calculate for next/previous navigation
+    prev_id = int(user_id) - 1
+    next_id = int(user_id) + 1
+    # ToDo: what if foo_id == 0 or nonexistant?
+    # maybe use template logic to not show prev-next-link?
+    # maybe try to figure out max_id? what is cheaper? just fail/404?
+    # we need the 404 anyway, if user picks random url/user_id
+
+    # show who is watching. maybe we should log this ;-)
+    viewer_username = authenticated_userid(request)
+
+    return {
+        'user': user,
+        'viewer_username': viewer_username,
+        'prev_id': prev_id,
+        'next_id': next_id,
+        }
+
+# formencode schema for user settings ####################################
+class UserSettingsSchema(formencode.Schema):
+    allow_extra_fields = True
+    filter_extra_fields = True
+#    username = formencode.All(validators.PlainText(not_empty = True),
+#                              UniqueUsername())
+#    new_password = formencode.validators.PlainText(not_empty = True)
+#    confirm_password =  formencode.validators.String(not_empty = True)
+    user_email = formencode.validators.Email(resolve_domain = False, not_empty=True)
+    user_surname =  formencode.validators.String(not_empty = True)
+    user_lastname =  formencode.validators.String(not_empty = True)
+#    password =  formencode.validators.String(not_empty = True)
+#    chained_validators = [
+#        formencode.validators.FieldsMatch('new_password', 'confirm_password')
+#        ]
+
+
+# #################################################################### user_edit
+# #from formencode import htmlfill
+# from c3sregistration.security import UserContainer
+
+# @view_config(route_name='user_edit', 
+#              #permission='view',
+#              permission='editUser',
+# #             context=UserContainer,
+#              renderer='../templates/user_edit_table.pt')
+# def user_edit(request):
+#     """
+#     let users change some of their details
+#     """
+#     session = getSession()
+
+#     user_id = request.matchdict['user_id']
+#     user = User.get_by_user_id(user_id)
+
+# #    request.session.flash("email confirmed? " + str(user.user_email_conf))
+#     if user.user_email_conf == True:
+#         email_is_confirmed = "Yes"
+#     else:
+#         email_is_confirmed = "No"
+
+#     form = Form(request, schema = UserSettingsSchema, obj = user)
+
+
+#     if form.validate():
+#         request.session.flash("Yes! form.validate() !!!")
+
+
+#     if 'form.submitted' in request.POST and not form.validate():
+#         # form didn't validate
+#         request.session.flash('form does not validate!')
+#         request.session.flash(form.data['user_surname'])
+#         request.session.flash(form.data['user_lastname'])
+#         request.session.flash(form.data['user_email'])
+
+#     if 'form.submitted' in request.POST and form.validate():
+#         # ready for registration!
+#         request.session.flash('form validated!')
+#         #session = DBSession()
+#         #username = form.data['username']
+
+#         # user = User(
+#         #     username = user.username,
+#         #     #password = form.data['password'],
+#         #     user_surname = form.data['user_surname'],
+#         #     user_lastname = form.data['user_lastname'],
+#         #     user_email = form.data['user_email']
+#         #     )
+
+#         if form.data['user_surname'] !=  user.user_surname:
+#             request.session.flash('surname was not same --> changing')
+#             user.user_surname = form.data['user_surname']
+
+#         if form.data['user_lastname'] !=  user.user_lastname:
+#             request.session.flash('lastname was not same --> changing')
+#             user.user_lastname = form.data['user_lastname']
+
+#         if form.data['user_email'] !=  user.user_email:
+#             request.session.flash('email was not same --> changing')
+#             user.user_email = form.data['user_email']
+
+# # ToDo
+#         # if form.data['user_telephone'] !=  user.user_telephone:
+#         #     request.session.flash('telephone was not same --> changing')
+#         #     user.user_telephone = form.data['user_telephone']
+
+#         # if form.data['user_telefax'] !=  user.user_telefax:
+#         #     request.session.flash('telefax was not same --> changing')
+#         #     user.user_telefax = form.data['user_telefax']
+
+
+
+#         #redirect_url = route_url('user_view', request)
+
+
+# #       return HTTPFound(location = redirect_url, headers=headers)
+#        # return dict (
+#        #     message = 'You won!'
+#        #     )
+
+
+#     return {
+#         'the_user_id': user_id,
+#         'the_username': user.username,
+#         'email_is_confirmed': email_is_confirmed,
+#         'form': FormRenderer(form),
+#         }
