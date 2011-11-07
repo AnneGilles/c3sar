f3464fea909e1ac3e7e48a8b069c59d4ce2163ba
Christoph Scheid <c@shri.de>
2011-11-07 02:33:32 +0100

    added view for reporting api

---
+++ c3sar/views/reporting_api.py
@@ -0,0 +1,23 @@
+from pyramid.request import Request
+from pyramid.response import Response
+
+from c3sar.models import (
+    DBSession,
+#    Report,
+    )
+
+
+#@view_config(route_name='api_report_single_airplay',
+#             request_method ='GET')
+def reporting_single_airplays_view(request):
+    """
+    this view is able to receive reporting data
+    via rest api call
+    """
+    from_matchdict = request.matchdict['the_report']
+    the_report = from_matchdict.encode('base64')
+
+#    dbsession = DBSession
+    print "got a new report! " +  str(the_report)
+    
+    return Response("Thanks! got: %s" % the_report.decode('base64'))
